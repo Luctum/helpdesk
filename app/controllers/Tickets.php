798a8e06972e33b867f11384b12d8b3843b7a0e7
Hugo PALLA <hugo.palla@gmail.com>
2015-09-28 11:48:06 +0200

    Revert "Revert "Resolving problem with xdebug and displayed errors""
    This reverts commit 544f88c8f0b6e061ede19d04486ade697e17fb36.

--- app/controllers/Tickets.php
+++ app/controllers/Tickets.php
@@ -37,8 +37,9 @@ class Tickets extends \_DefaultController {
 					echo "<td>".$object->getUser()."</td>";
 				}
 				if($object->getNotif() == 0){
-					xdebug_disable();
 					error_reporting(0);
+					xdebug_disable();
+					
 					echo "<td class='btn-warning'></td>";
 	
 					$object->setNotif(1);
@@ -100,9 +101,10 @@ class Tickets extends \_DefaultController {
 			if($msg->getLu() == 0 && Auth::getUser() != $msg->getUser()){
 				echo "<span class='msg-new btn btn-warning'>NEW</span>";
 
-			}        
-			xdebug_disable();
+			}
 			error_reporting(0);
+			xdebug_disable();
+			
 			echo "Le ".$msg->getDate().", ".$msg->getUser()." dit : ";                                                                                       
 			echo "</div>";                                                                                                                                                       
 			echo "<div class='msg-text'>";                                                                                                                                   
