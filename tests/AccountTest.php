5f07ea989859e2cec91a1fa3feaf606cea62c212
Luctum <hugo.palla@gmail.com>
2015-10-11 20:27:14 +0200

    Logout and Login test, correcting older test

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -16,8 +16,8 @@ class AccountTest extends AjaxUnitTest {
 		//L'utilisateur clique sur Editer mon profil
 		$this->get("DefaultC/index");
 		self::$webDriver->manage()->timeouts()->implicitlyWait(5);
-		$btnEdit=$this->getElementsBySelector("btn .btn-primary");
-		
+		$btnEdit=$this->getElementsBySelector(".btn .btn-primary");
+
 		foreach ($btnEdit as $bt){
 			if($bt->getText()=="profil"){
 				$bt->click();
@@ -27,11 +27,11 @@ class AccountTest extends AjaxUnitTest {
 				$this->setField("mail", "user1@local.fr");
 				$this->clickSubmit("Valider");
 				self::$webDriver->manage()->timeouts()->implicitlyWait(5);
-				$test = $DAO::getOne("User","login='user'");
+				$test = DAO::getOne("User","login='user'");
 				$mail = $test->getMail();
 				$this->assert($mail, Auth::getUser()->getMail());
 			}
-		
+
 		}
 		//L'utilisateur essaie d'accéder à un profil qui n'est pas le sien (user = 2, test sur le 4éme)
 		$this->get("Users/frm/4");
@@ -39,5 +39,29 @@ class AccountTest extends AjaxUnitTest {
 		$this->assertPageContainsText("Vous ne disposez pas des droits");
 		$_SESSION['user'] = null;
 	}
-	
+
+    public function testConnexion(){
+        $this->get("DefaultC/index");
+        self::$webDriver->manage()->timeouts()->implicitlyWait(5);
+        $this->assertPageContainsText("Se connecter");
+        $this->setField("mail", "admin");
+        $this->setField("password", "admin");
+        $this->clickSubmit("Valider");
+        self::$webDriver->manage()->timeouts()->implicitlyWait(5);
+        $this->assert(Auth::getUser()->getLogin(), "admin");
+    }
+
+    public function testDeconnexion(){
+        $user = DAO::getOne("User","login='user'");
+        $_SESSION["user"]= $user;
+        $this->get("DefaultC/index");
+        self::$webDriver->manage()->timeouts()->implicitlyWait(5);
+        $this->assertPageContainsText("Déconnexion");
+        $btDeco=$this->getElementBySelector(".btn-warning");
+        $this->click($btDeco);
+        self::$webDriver->manage()->timeouts()->implicitlyWait(5);
+        $this->get("DefaultC/index");
+        $this->assert(Auth::getUser, NULL);
+    }
+
 }
