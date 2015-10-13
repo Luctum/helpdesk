a95abb3a447bdaf0330bf28d703c6ac216ea73f8
Hugo PALLA <hugo.palla@gmail.com>
2015-10-13 16:39:25 +0200

    adding use of database for travis ci

--- app/database/helpdesk.sql
+++ app/database/helpdesk.sql
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `helpdesk`
 --
+CREATE DATABASE IF NOT EXISTS `helpdesk` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `helpdesk`;
 
 -- --------------------------------------------------------
 
